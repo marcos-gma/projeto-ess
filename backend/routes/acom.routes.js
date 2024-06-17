d3f8f2a02382b55a0bf2f9f4d14432bbdcc783a3
arthurrlima <arl3@cin.ufpe.br>
2024-06-17 08:44:30 -0300

    Add routes and controller for Edit Accommodation feature

--- backend/routes/acom.routes.js
+++ backend/routes/acom.routes.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import { publishAccommodation } from '../controllers/acomController.js';
+import { editAccommodation } from '../controllers/acomController.js';
 
 const router = express.Router();
 
-//@TODO double-check router rout
+//@TODO double-check router routes
 router.post('/host/accommodations', publishAccommodation);
+router.put('/host/accommodations/:id', editAccommodation);
 
 export default router;
