89ecc9485fd2c76b7119fdb9bade32f659d9c104
arthurrlima <arl3@cin.ufpe.br>
2024-06-17 09:09:19 -0300

    Add routes and controller for Cancel Reservation Feature

--- backend/controllers/reservationController.js
+++ backend/controllers/reservationController.js
@@ -37,3 +37,41 @@ export const listAccommodationReservations = async (req, res) => {
         });
     }
 };
+
+// Função para cancelar uma reserva
+export const cancelReservation = async (req, res) => {
+    try {
+        const { userId, reservationId } = req.params;
+
+        if (!userId || !reservationId) {
+            console.log("User ID and Reservation ID are required");
+            return res.status(400).json({
+                error: "User ID and Reservation ID are required"
+            });
+        }
+
+        var reservationsData = JSON.parse(fs.readFileSync(path.resolve('./samples/reservations.json'), 'utf8'));
+
+        const reservationIndex = reservationsData.findIndex(reservation => reservation.id === reservationId && reservation.userId === userId);
+        if (reservationIndex === -1) {
+            console.log("Reservation not found or does not belong to the user");
+            return res.status(404).json({
+                error: "Reservation not found or does not belong to the user"
+            });
+        }
+
+        reservationsData.splice(reservationIndex, 1);
+
+        fs.writeFileSync(path.resolve('./samples/reservations.json'), JSON.stringify(reservationsData, null, 2));
+
+        res.status(200).json({
+            message: "Reservation cancelled successfully"
+        });
+
+    } catch (error) {
+        console.log("Error in cancelReservation controller:", error.message);
+        res.status(500).json({
+            error: "Internal Server Error"
+        });
+    }
+};
