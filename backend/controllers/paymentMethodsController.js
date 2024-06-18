bb385abea62a0e1e5022ac0006400653737fcba2
iasmin <iasmin.ufpe@gmail.com>
2024-06-18 00:57:01 -0300

    (feat): payment methods controller and routes

---
+++ backend/controllers/paymentMethodsController.js
@@ -0,0 +1,166 @@
+import fs from 'fs';
+import path from 'path';
+
+
+export const visualize = async (req, res) => {
+    try {
+        const { id } = req.body;
+
+        var data = JSON.parse(fs.readFileSync(path.resolve('./samples/users.json'), 'utf8'));
+        const user = data.find(element => element.id == id);
+
+        if (user.cards.length > 0) {
+            user.cards.forEach(element => {
+                console.log(element.cardNumber);
+                console.log(element.type);
+            });
+        }
+        else console.log("");
+        res.status(200).json();
+    }
+    catch (error) {
+        console.log("Error in visualize controller: ", error.message);
+        res.status(500).json({
+            error: "Internal Server Error"
+        });
+    };
+    
+};
+
+export const add =  async (req, res) => {
+    try {
+        const { id, cardNumber, name, expireDate, type, cvv } = req.body;
+
+        // Validation
+        // Empty field
+        if (!cardNumber || !name || !expireDate || !type || !cvv) {
+            console.log("All fields are required")
+            return res.status(400).json({
+                error: "All fields are required"
+            });
+        }
+
+        // Size and isDigit
+        const cardNumberRegex = /^\d{16}$/;
+        if (!cardNumberRegex.test(cardNumber)) {
+            console.log("Card number must have sixteen digits and contain only numbers");
+            return res.status(400).json({
+                error: "Card number must have sixteen digits and contain only numbers"
+            }); 
+        }
+
+        const cvvRegex = /^\d{3}$/;
+        if (!cvvRegex.test(cvv)) {
+            console.log("CVV must have three digits and contain only numbers");
+            return res.status(400).json({
+                error: "CVV must have three digits and contain only numbers"
+            });
+        }
+
+        // mm/dd/yyy format and isDigit
+        const dateRegex = /^(0[1-9]|1[0-2])\/(0[1-9]|[12][0-9]|3[01])\/\d{4}$/;
+        const currentDate = new Date();
+        const inputDate = new Date(expireDate);
+        if (!dateRegex.test(expireDate) || inputDate < currentDate) {
+            console.log("Expire date is not valid");
+            return res.status(400).json({
+                error: "Expire date is not valid"
+            });
+        }
+
+        // Debit or credit
+        if (type != "debit" && type != "credit") {
+            console.log("Card must be of type debit or credit");
+            return res.status(400).json({
+                error: "Card must be of type debit or credit"
+            });
+        }
+
+        // isUnique
+        var data = JSON.parse(fs.readFileSync(path.resolve('./samples/users.json'), 'utf8'));
+        const user = data.find(element => element.id == id);
+        if (user && user.cards) {
+            const existingCard = user.cards.find(element => element.cardNumber == cardNumber && element.type == type);
+            if (existingCard) {
+                console.log("Card already registered");
+                return res.status(400).json({
+                    error: "Card already registered"
+                });
+            };
+        }
+
+        // Add new payment method
+        user.cards.push({ cardNumber, type });
+        fs.writeFileSync(path.resolve('./samples/users.json'), JSON.stringify(data, null, 2));
+        res.status(201).json({
+            cardNumber,
+            type
+        });
+
+    }
+    catch (error) {
+        console.log("Error in add controller: ", error.message);
+        res.status(500).json({
+            error: "Internal Server Error"
+        });
+    };
+};
+
+export const remove = async (req, res) => {
+    try {
+        const { id, cardNumber, type } = req.body;
+        var data = JSON.parse(fs.readFileSync(path.resolve('./samples/users.json'), 'utf8'));
+        const userIndex = data.findIndex(element => element.id === id);
+        const user = data[userIndex];
+
+        // Validation
+        // Existing card
+        const existingCard = user.cards.find(element => element.cardNumber === cardNumber && element.type === type);
+        if (!existingCard) {
+            console.log("Card not found");
+            return res.status(404).json({
+                error: "Card not found"
+            });
+        }
+
+        // Remove card
+        user.cards = user.cards.filter(element => !(element.cardNumber === cardNumber && element.type === type));
+        data[userIndex] = user;
+        fs.writeFileSync(path.resolve('./samples/users.json'), JSON.stringify(data, null, 2));
+        return res.status(204).json();
+    }
+    catch (error) {
+        console.log("Error in remove controller: ", error.message);
+        res.status(500).json({
+            error: "Internal Server Error"
+        });
+    };
+};
+
+export const select = async (req, res) => {
+    try {
+        const { id, cardNumber, type } = req.body;
+        var data = JSON.parse(fs.readFileSync(path.resolve('./samples/users.json'), 'utf8'));
+        const user = data.filter(element => element.id === id);
+
+        // Validation
+        // Existing card
+        const existingCard = user.cards.find(card => card.cardNumber == cardNumber && card.type == type);
+
+        if (!existingCard) {
+            console.log("Card does not exist and can not be selected");
+            res.status(400).json({
+                error: "Card does not exist and can not be selected"
+            })
+        }
+
+        // Select card
+
+    }
+    catch (error) {
+        console.log("Error in select controller: ", error.message);
+        res.status(500).json({
+            error: "Internal Server Error"
+        });
+    };
+}
--- backend/index.js
+++ backend/index.js
@@ -4,37 +4,18 @@ import cookieParser from 'cookie-parser';
 
 const app = express();
 
-app.use(express.json())
-dotenv.config()
-app.use(cookieParser())
+app.use(express.json());
+dotenv.config();
+app.use(cookieParser());
 
 //routes imports
 
-import pingRoutes from './routes/ping.routes.js'
-import authRoutes from './routes/auth.routes.js'
-import emailRoutes from './routes/email.routes.js'
-import promoRoutes from './routes/promo.routes.js';
+import pingRoutes from './routes/ping.routes.js';
+import authRoutes from './routes/auth.routes.js';
+import paymentMethodsRoutes from './routes/paymentMethods.routes.js';
 
-//rating and reservation routes
-import rateRoutes from './routes/rate.routes.js'
-
-//accomodation routes
-import accommodationRoutes from './routes/acom.routes.js';
-//@TODO doublecheck routes
-app.use('/user', accommodationRoutes);
-
-//reservation routes
-import reservationRoutes from './routes/reservation.routes.js';
-//@TODO doublecheck routes
-app.use('/user', reservationRoutes);
 app.use('/ping', pingRoutes);
 app.use('/auth', authRoutes);
-app.use('/email', emailRoutes);
-app.use('/promo', promoRoutes);
-app.use('/user', rateRoutes);
+app.use('/payment-methods', paymentMethodsRoutes);
 
-app.listen(5001, () => {
-    console.log("\nServer is listening on port 5001");
-});
-
-export default app;
+app.listen(5001);
