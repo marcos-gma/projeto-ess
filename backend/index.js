a84729f24212621d3d3b5f435e86b2f05bd844b7
Felipe Durant <fdf@cin.ufpe.br>
2024-06-17 21:49:44 -0300

    fixes Rating and reservation

--- backend/index.js
+++ backend/index.js
@@ -2,9 +2,6 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 
-import dotenv from 'dotenv';
-import express from 'express';
-
 const app = express();
 
 app.use(express.json())
--- backend/routes/rate.routes.js
+++ backend/routes/rate.routes.js
@@ -1,8 +1,8 @@
 import express from 'express';
-import { createRating } from '../controllers/ratingController';
+import { createRating } from '../controllers/ratingController.js';
 
 const router = express.Router();
 
-router.post('/guest/reservations/avaliacao/:idRating', createRating);
+router.post('/guest/reservations/avaliacao', createRating);
 
 export default router
