8b516e7f0f32fbfcdbd5e61b8111e37f41a25bd5
iasmin <iasmin.ufpe@gmail.com>
2024-06-28 15:45:28 -0300

    quick fixes

--- backend/controllers/pagamentoController.js
+++ backend/controllers/pagamentoController.js
@@ -57,7 +57,7 @@ export const add =  async (req, res) => {
             });
         }
 
-        // mm/dd/yyy format and isDigit
+        // mm/dd/yyyy format and isDigit
         const dateRegex = /^(0[1-9]|1[0-2])\/(0[1-9]|[12][0-9]|3[01])\/\d{4}$/;
         const currentDate = new Date();
         const inputDate = new Date(expireDate);
