4670058489fde8f59afb3a8aa0a07f680bc94770
gabrielrochass <120466775+gabrielrochass@users.noreply.github.com>
2024-06-30 16:37:01 -0300

    style: remove comments from promoController

--- backend/controllers/promoController.js
+++ backend/controllers/promoController.js
@@ -1,9 +1,8 @@
-// variáveis de promoção: desconto, promoName, data_inicio, data_fim
+// variáveis de promoção: desconto, promoName, promoId, data_inicio, data_fim
 
 import fs from 'fs';
 import path from 'path';
 
-import { v4 as uuidv4 } from 'uuid';
 
 function withDiscount(original, desconto) {
     return original * (1 - (desconto/100)); // retorna o valor com desconto
@@ -54,7 +53,7 @@ export const createPromo = (req, res) => {
 
         hotel.desconto = desconto;
         hotel.promoName = promoName;
-        hotel.promoId = uuidv4(); 
+        hotel.promoId = id;
         
         if (!validateDate(data_inicio, data_fim)) { 
             return res.status(400).json({ error: 'Invalid date. Final date should be after the beginning promotion date.' });
@@ -63,9 +62,6 @@ export const createPromo = (req, res) => {
         hotel.data_inicio = data_inicio;
         hotel.data_fim = data_fim;
 
-        // data[hotelIndex] = hotel;
-        // delete data[hotelIndex];
-        // data.push(hotel);
         fs.writeFileSync(path.resolve('./samples/accommodations.json'), JSON.stringify(data, null, 2))
         res.status(200).json({ message: 'Promo created successfully.' }); // retorna o hotel atualizado
     
@@ -96,28 +92,29 @@ export const listPromos = (req, res) => {
 export const deletePromo = (req, res) => {
     try {
         const { id } = req.params; // pega o id do hotel
-        console.log(`Deleting promo for hotel with id: ${id}`); // log the id for debugging
+        console.log(`Deleting promo for hotel with id: ${id}`); 
         const hotelIndex = data.findIndex(hotel => String(hotel.id) === String(id));
 
         if (hotelIndex === -1) { // verifica se o hotel existe
-            console.log(`Hotel with id: ${id} not found`); // log the error for debugging
+            console.log(`Hotel with id: ${id} not found`); 
             return res.status(404).json({ error: 'Hotel not found.' });
         }
 
-        // Remover as chaves relacionadas à promoção
+        // remove as chaves relacionadas à promoção
         delete data[hotelIndex].promoName;
         delete data[hotelIndex].desconto;
         delete data[hotelIndex].data_inicio;
         delete data[hotelIndex].data_fim;
         delete data[hotelIndex].promoId;
 
+        // escreve no arquivo
         fs.writeFileSync(path.resolve('./samples/accommodations.json'), JSON.stringify(data, null, 2));
 
-        console.log(`Promo deleted for hotel with id: ${id}`); // log the success for debugging
+        console.log(`Promo deleted for hotel with id: ${id}`);
         res.status(200).json({ message: 'Promo deleted successfully.' });
     
     } catch (error) {
-        console.log(`Error deleting promo for hotel with id: ${id}: ${error.message}`); // log the error for debugging
+        console.log(`Error deleting promo for hotel with id: ${id}: ${error.message}`); 
         return res.status(400).send({ message: error.message });
     }
 };
