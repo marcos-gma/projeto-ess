78b5a82d7712560f4f09f372e945b88e2230622e
iasmin <iasmin.ufpe@gmail.com>
2024-07-01 21:50:09 -0300

    add payment method test

--- backend/tests/Pagamento/adicionar.test.js
+++ backend/tests/Pagamento/adicionar.test.js
@@ -18,7 +18,7 @@ defineFeature(feature, test => {
 
         });
 
-        when(/^uma nova requisição POST é feita com email: "(.*)", cardNumber: "(.*)", name: "(.*)", expireDate: "(.*)", type: "(.*)" e cvv: "(.*)"$/, async (endereco, cartao, nome, validade, tipo, seg) => {
+        when(/^uma nova requisição POST é feita com e-mail: "(.*)", cardNumber: "(.*)", name: "(.*)", expireDate: "(.*)", type: "(.*)" e cvv: "(.*)"$/, async (endereco, cartao, nome, validade, tipo, seg) => {
             const url = '/pagamento/add';
 
             response = await request.post(url).send({
@@ -40,6 +40,40 @@ defineFeature(feature, test => {
 
             expect(cardNumber).toBe(cartao);
             expect(type).toBe(tipo);
+        });
+    });
+
+    test('Adicionar novo método de pagamento sem sucesso', ({ given, and, when, then }) => {
+        let response;
+
+        given(/^o usuário com e-mail "(.*)" está cadastrado no sistema$/, (email) => {
+
+        });
+
+        and(/^não há o cartão com cardNumber "(.*)" e type "(.*)" registrado no seu cadastro$/, (cardNumber, type) => {
+
+        });
+
+        when(/^uma nova requisição POST é feita com e-mail: "(.*)", cardNumber: "(.*)", name: "(.*)", expireDate: "(.*)", type: "(.*)" e cvv: "(.*)"$/, async (email, cardNumber, name, expireDate, type, cvv) => {
+            const url = '/pagamento/add';
+
+            response = await request.post(url).send({
+                email: email, 
+                cardNumber: cardNumber, 
+                name: name, 
+                expireDate: expireDate, 
+                type: type, 
+                cvv: cvv
+            });
+        });
+
+        then(/^o cartão com cardNumber "(.*)" e type "(.*)" não é registrado no seu cadastro$/, (cardNumber, type) => {
+
+        });
+
+        and(/^o código de resposta é "(.*)"$/, (anscode) => {
+            expect(response.status).toBe(400);
+
         });
     });
 });
