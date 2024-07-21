087bec8336a9a7de794420214aa50e5a3c724309
gabrielrochass <120466775+gabrielrochass@users.noreply.github.com>
2024-07-20 21:40:56 -0300

    feat: first modal de new promo

--- frontend/src/app/home/pages/promo/myPromos/newPromo.js
+++ frontend/src/app/home/pages/promo/myPromos/newPromo.js
@@ -1,44 +1,77 @@
-import React from "react";
+import React, { useState } from "react";
+import Modal from "react-modal";
 import Button from "../../Compartilhado/button.js";
 import NavBar from "../../Compartilhado/navbar.js";
 
-const NewPromo = () => {
-    return (
-        <div>
-        <NavBar />
-        <div className="main">
-            <h1>Create a new promo</h1>
-            <form>
-            <label>
-                Nome da Promoção:
-                <input type="text" name="promoName" />
-            </label>
-            <br></br>
-            <label>
-                Nome do Hotel:
-                <input type="text" name="nome" />
-            </label>
-            <br></br>
-            <label>
-                Desconto:
-                <input type="number" name="desconto" />
-            </label>
-            <br></br>
-            <label>
-                Data de Início:
-                <input type="date" name="data_inicio" />
-            </label>
-            <br></br>
-            <label>
-                Data de Fim:
-                <input type="date" name="data_fim" />
-            </label>
-            <br></br>
-            <Button nome="Create" destino="/my-promos" />
-            </form>
-        </div>
-        </div>
-    );
-};  
+Modal.setAppElement('#root'); 
 
-export default NewPromo;
+const NewPromoModal = () => {
+  const [modalIsOpen, setModalIsOpen] = useState(false); // Hook para abrir e fechar o modal
+
+  const openModal = () => {
+    setModalIsOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalIsOpen(false);
+  };
+
+  return (
+    <div>
+      <NavBar />
+      <div className="main">
+        <button onClick={openModal}>Create a new promo</button>
+        <Modal
+          isOpen={modalIsOpen}
+          onRequestClose={closeModal}
+          style={{
+            overlay: {
+              backgroundColor: 'rgba(0, 0, 0, 0.5)',
+            },
+            content: {
+              top: '50%',
+              left: '50%',
+              right: 'auto',
+              bottom: 'auto',
+              marginRight: '-50%',
+              transform: 'translate(-50%, -50%)',
+            },
+          }}
+        >
+          <h1>Create a new promo</h1>
+          <form>
+            <label>
+              Nome da Promoção:
+              <input type="text" name="promoName" />
+            </label>
+            <br />
+            <label>
+              Nome do Hotel:
+              <input type="text" name="nome" />
+            </label>
+            <br />
+            <label>
+              Desconto:
+              <input type="number" name="desconto" />
+            </label>
+            <br />
+            <label>
+              Data de Início:
+              <input type="date" name="data_inicio" />
+            </label>
+            <br />
+            <label>
+              Data de Fim:
+              <input type="date" name="data_fim" />
+            </label>
+            <br />
+            <Button nome="Create" destino="/my-promos" />
+          </form>
+          <button onClick={closeModal}>Close</button>
+        </Modal>
+      </div>
+    </div>
+  );
+};
+
+export default NewPromoModal;
