50a8b9b8b5877161359d7ef8c1d69329896f4088
gabrielrochass <120466775+gabrielrochass@users.noreply.github.com>
2024-07-22 23:47:53 -0300

    fix: cypress rodando

--- frontend/cypress.config.ts
+++ frontend/cypress.config.ts
@@ -1,41 +1,13 @@
-import cucumberPreprocessor from '@badeball/cypress-cucumber-preprocessor'
-import { createEsbuildPlugin } from '@badeball/cypress-cucumber-preprocessor/esbuild'
-import createBundler from '@bahmutov/cypress-esbuild-preprocessor'
-import { defineConfig } from 'cypress'
+const cucumber = require('cypress-cucumber-preprocessor').default
+const { defineConfig } = require('cypress')
 
-export default defineConfig({
-  video: false,
-  viewportWidth: 1280,
-  viewportHeight: 720,
-  defaultCommandTimeout: 5000,
-  execTimeout: 60000,
-  retries: {
-    runMode: 1,
-    openMode: 0
-  },
-  chromeWebSecurity: false,
-  env: {
-    codeCoverage: {
-      exclude: ['cypress/**/*.*', 'coverage/**/*.*']
-    }
-  },
+module.exports = defineConfig({
   e2e: {
-    async setupNodeEvents(on: Cypress.PluginEvents, config: Cypress.PluginConfigOptions): Promise<Cypress.PluginConfigOptions> {
-      await cucumberPreprocessor.addCucumberPreprocessorPlugin(on, config)
-
-      on(
-        'file:preprocessor',
-        createBundler({
-          plugins: [createEsbuildPlugin(config)]
-        })
-      )
-
-      // TODO: Fix coverage
-      // coverageTask(on, config);
-
-      return config
+    setupNodeEvents(on) {
+      on('file:preprocessor', cucumber({
+        typescript: require.resolve('typescript')
+      }))
     },
-    baseUrl: 'http://localhost:3000',
-    specPattern: 'cypress/e2e/**/*.feature'
+    specPattern: 'cypress/e2e/features/*.feature',
   }
 })
